ca7ba1a9ad24a42420794823fbc6a9c153fdfd2a
jdsteinman <johnsteinman@utexas.edu>
2021-01-29 14:41:00 -0600

    New paraview

--- post/ellipsoid/shear_gradient.py
+++ post/ellipsoid/shear_gradient.py
@@ -3,7 +3,7 @@ import numpy as np
 import math
 
 mu = 325 
-K = [0, 0.5, 1, 2]
+K = [0, 0.25, 1, 4]
 x = np.linspace(-0, 200, 1000)
 y = np.ones((len(K), len(x)))
 
@@ -12,7 +12,6 @@ c = ['g', 'gold', 'r', 'b']
 for i, k in enumerate(K):
     for j, r in enumerate(x):
         if r <= 150:
-            print(mu * (r/150) ** k)
             y[i, j] = mu * (r/150) ** k
         else:
             y[i, j] = mu
