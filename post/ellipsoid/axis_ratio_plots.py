f95b36dfb7f6341d8dff68a4353fc6f409d1c857
jdsteinman <johnsteinman@utexas.edu>
2021-03-07 21:01:40 -0600

    post

--- post/ellipsoid/axis_ratio_plots.py
+++ post/ellipsoid/axis_ratio_plots.py
@@ -1,59 +1,45 @@
 import numpy as np
-import  matplotlib.pyplot as plt
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import math
 
-## Data
-path = "../../gel_model/output/"
-out_path = "/home/john/Pictures/2-5-21/"
-axis = "z"
+# Inputs
+data1 = pd.read_csv("../../gel_model/output/uniform/data_z.csv", index_col=0)
+data2 = pd.read_csv("../../gel_model/output/step/data_z.csv", index_col=0)
 
-u_unif = np.loadtxt(path + "uniform/disp_" + axis + "_uniform.txt")
-mu_unif = np.loadtxt(path + "uniform/mu_" + axis + "_uniform.txt")
-u_step = np.loadtxt(path + "step/disp_" + axis + "_step.txt")[:, 3:]
-mu_step = np.loadtxt(path + "step/mu_" + axis + "_step.txt")[:,-1]
+output_folder = "/home/john/Pictures/2-12-21/"
 
-u_unif = np.sum(u_unif**2, axis=1)**0.5
-u_step = np.sum(u_step**2, axis=1)**0.5
+params = ["F33", "C33", ]
+title = "Ratio along Z-axis"
+xlab = "Distance from cell surface"
+leg = ['uniform', 'step']
 
-u_rat = u_step / u_unif
-mu_rat = mu_step / mu_unif
+# Plot setup
+sns.set_theme(style="darkgrid")
 
-npoints = 1000
-points = np.linspace(20, 100, npoints) - 20
+# Displacement
+fig, ax = [], []
 
-## Plots
-fig, ax = plt.subplots(1,1)
-ax.plot(points, u_rat, label="u ratio")
-ax.plot(points, mu_rat, label="mu ratio")
+for i, y in enumerate(params):
+    f, a = plt.subplots(1,1)
 
-ax.set_title("Ratios along " + axis + "-axis")
-ax.set_xlabel(r'Distance from surface ($\mu$m)')
-ax.set_ylabel("Step/Uniform")
-ax.set_ylim(0, 1.25)
-ax.grid()
-ax.legend()
+    ratio = data1.loc[:, y] / data2.loc[:, y]
+    sns.lineplot(x=data1.r, y=ratio, ci=None, ax = a, label=leg[0] + " / " + leg[1])
 
-fig2, ax = plt.subplots(1,1)
-ax.plot(points, u_step, label="step", c='r')
-ax.plot(points, u_unif, label="uniform", c='b')
+    if y == "mu": a.set_ylim(0, 350)
 
-ax.set_title("Displacement Magnitude along " + axis + "-axis")
-ax.set_xlabel(r'Distance from surface ($\mu$m)')
-ax.set_ylabel("Displacement ($\mu$m)")
-ax.grid()
-ax.legend()
+    a.vlines(x=10, ymin=a.get_ylim()[0], ymax=a.get_ylim()[1], linestyles='dashed')
 
-fig3, ax = plt.subplots(1,1)
-ax.plot(points, mu_step, label="step", c='r')
-ax.plot(points, mu_unif, label="uniform", c='b')
+    a.set_title(y + " " + title)
+    a.set_ylabel(y)
+    a.set_xlabel(xlab)
+    a.legend(loc='best')
 
-ax.set_title("Shear modulus along " + axis + "-axis")
-ax.set_xlabel(r'Distance from surface ($\mu$m)')
-ax.set_ylabel("Pascals (Pa)")
-ax.set_ylim(0, 350)
-ax.grid()
-ax.legend()
+    # f.savefig(output_folder + y + ".png")
 
-fig.savefig(out_path + "ratios_" + axis + ".png")
-fig2.savefig(out_path + "disp_" + axis + ".png")
-fig3.savefig(out_path + "shear_" + axis + ".png")
+    fig.append(f)
+    ax.append(a)
+
+plt.tight_layout()
 plt.show()
