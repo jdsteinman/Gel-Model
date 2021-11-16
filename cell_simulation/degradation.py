03da0f9631b046770f82c10639df0f88119d4eaf
John <johnsteinman@utexas.edu>
2021-11-16 16:11:45 -0600

    deg

--- cell_simulation/degradation.py
+++ cell_simulation/degradation.py
@@ -3,7 +3,6 @@ import os
 import argparse
 import numpy as np
 import pandas as pd
-
 import meshio
 
 def main(folder):
@@ -15,29 +14,43 @@ def main(folder):
     u_file = h5py.File(os.path.join(folder, "u.h5"), 'r')
     u_sim = u_file['VisualisationVector']['0'][()]
     u_sim = np.array(u_sim)
+    u_sim_mag = np.sum(u_sim**2, axis=1)**0.5
 
     # Experimental data
     u_data = pd.read_csv("./output/single_field/coarse/interpolated_data.csv", index_col=False)
     u_data = -1*u_data.loc[:,'u':'w'].to_numpy()
+    u_data_mag = np.sum(u_data**2, axis=1)**0.5
 
     # Degradation
-    Dmax = 0.5
     degradation = np.zeros(u_sim.shape[0])
-    discepancy = u_data-u_sim
-    dpos = discepancy[discepancy>0]
-    dneg = discepancy[discepancy<0]
-    pos_ind = np.where(discepancy>0)
-    neg_ind = np.where(discepancy<0)
-    dpos_scaled = (dpos-np.min(dpos))/(np.max(dpos)-np.min(dpos)) * Dmax
-    dneg_scaled = (dneg-np.min(dneg))/(np.max(dneg)-np.min(dneg)) * -Dmax
-    degradation[pos_ind] = dpos_scaled
-    degradation[neg_ind] = dneg_scaled
+    discrepancy = u_data_mag-u_sim_mag
+    max_d = np.max(discrepancy)
+    # print(max_d)
+    # degradation = discrepancy * 0.999/4
 
-    meshio.write(os.path.join(folder, "degradation.xdmf"),
-        points=mesh.points,
-        cells=mesh.cells,
-        point_data=degradation
+    Dmax = 0.9
+    degradation = np.zeros(u_sim.shape[0])
+    discrepancy = u_data_mag-u_sim_mag
+    pos_ind = np.where(discrepancy>=0)[0]   # underestimation
+    dpos = discrepancy[pos_ind]
+    neg_ind = np.where(discrepancy<0)[0]    # overestimation
+    dneg = discrepancy[neg_ind]
+
+    degradation[pos_ind] = dpos * 0.999/5
+    # degradation[neg_ind] = dneg_scaled
+    degradation[neg_ind] = 0
+
+    mesh_out = meshio.Mesh(
+        mesh.points,
+        mesh.cells,
+        point_data = {"degradation":degradation}
     )
+
+    mesh_out.write(
+        os.path.join(folder, "degradation.xdmf")
+    )
+
+    np.savetxt(os.path.join(folder, "degradation.txt"), degradation)
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description="Postprocessing")
